9f8fb9da39bcfa5e8a53dd83f92c79a181979313
Friedrich Lindenberg <friedrich@pudo.org>
2018-05-03 21:30:43 +0200

    Simplify http code.

--- unoservice/api.py
+++ unoservice/api.py
@@ -6,10 +6,10 @@ from tempfile import mkstemp
 
 from unoservice.convert import FORMATS, PdfConverter
 from unoservice.exceptions import SystemFailure, ConversionFailure
-from unoservice.util import extract_extension
+from unoservice.util import normalize_extension
 
 ERROR_BUSY = 503
-ERROR_PERM = 406
+ERROR_INVALID = 400
 
 logging.basicConfig(level=logging.DEBUG)
 app = Flask(__name__)
@@ -35,28 +35,28 @@ def index():
 @app.route('/convert', methods=['PUT', 'POST'])
 def convert():
     acquired = lock.acquire(blocking=False)
-    fd, upload_file = mkstemp()
-    output_file = None
     if not acquired:
         return ('', ERROR_BUSY)
+    fd, upload_file = mkstemp()
+    output_file = None
     try:
         if 'file' not in request.files:
-            return ('', 400)
+            return ('', ERROR_INVALID)
         upload = request.files['file']
         upload.save(upload_file)
-        extension = extract_extension(upload.filename)
+        extension = normalize_extension(upload.filename)
         extension = request.form.get('extension', extension)
         mime_type = request.form.get('mime_type')
         output_file = converter.convert_file(upload_file, extension, mime_type)
         if not os.path.exists(output_file):
-            return ('', ERROR_PERM)
+            return ('', ERROR_INVALID)
         if os.path.getsize(output_file) == 0:
-            return ('', ERROR_PERM)
+            return ('', ERROR_INVALID)
         return send_file(output_file, mimetype='application/pdf')
     except SystemFailure:
         return ('', ERROR_BUSY)
     except ConversionFailure:
-        return ('', ERROR_PERM)
+        return ('', ERROR_INVALID)
     finally:
         lock.release()
         if upload_file is not None and os.path.exists(upload_file):
--- unoservice/util.py
+++ unoservice/util.py
@@ -1,4 +1,3 @@
-import os
 from celestial import normalize_extension as _normalize_extension
 
 PDF_FILTERS = (
@@ -8,13 +7,6 @@ PDF_FILTERS = (
     ("com.sun.star.presentation.PresentationDocument", "impress_pdf_Export"),
     ("com.sun.star.drawing.DrawingDocument", "draw_pdf_Export"),
 )
-
-
-def extract_extension(file_name):
-    if file_name is None:
-        return
-    _, extension = os.path.splitext(file_name)
-    return normalize_extension(extension)
 
 
 def normalize_extension(extension):
