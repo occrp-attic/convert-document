b601f3173ca56c861a9b2fcebe2ee38b4ac5c0de
Friedrich Lindenberg <friedrich@pudo.org>
2019-01-27 11:31:17 +0100

    Bump version: 1.3.0 → 1.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='convert',
-    version='1.3.0',
+    version='1.3.1',
     packages=find_packages(exclude=[]),
     install_requires=[
         'aiohttp',
